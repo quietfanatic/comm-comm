77d9d3da3c680948f6d89a4ea8fb4a8107fafc73
johnny4shure <davidshure79@gmail.com>
2012-10-03 20:59:54 -0700

    Initial Rails Gen

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CommComm::Application.config.secret_token = '24183a2a38dae795c84be1b8bd164a0fc17f06d63660983db5c0af9fde369fa3e077b59c50c3c89a846b67b5c8cb4280da488d9e0e0803aaa709a5521c62ab70'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+CommComm::Application.config.session_store :cookie_store, key: '_comm-comm_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# CommComm::Application.config.session_store :active_record_store
