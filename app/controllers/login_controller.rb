5ae63414a49c962ed4d781ba08e91a10d5add562
quietfanatic <lewis@wall.org>
2012-10-10 22:32:11 -0700

    Check if email and password are correct

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -2,7 +2,14 @@ class LoginController < ApplicationController
   def entrance
   end
   def login
-    unless User.find_by_email(params["email"])
+    user = User.find_by_email(params["email"])
+    if user
+      if user.password == params["password"]
+        @redirect = "/main/topic"
+      else
+        @redirect = "/login/entrance?state=incorrect"
+      end
+    else
       new_user = User.new(email: params["email"])
       new_user.password = params["password"]
       new_user.save!
