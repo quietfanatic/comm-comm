3594f11670142a92ee7b875d1f87b7351162c52d
johnny4shure <davidshure79@gmail.com>
2012-10-10 22:02:47 -0700

    Fixing login controller

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -3,7 +3,7 @@ class LoginController < ApplicationController
   end
   def login
     unless User.find_by_email(params["email"])
-      new_user = User.create(email: params["email"], password: params["password"])
+      new_user = User.create(email: params["email"], password_digest: params["password"])
     end
   end
 end
