79eea1fcd401235194ae8c785e90446e70822684
johnny4shure <davidshure79@gmail.com>
2012-10-10 21:45:44 -0700

    Basic authentication and friendz

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -2,5 +2,8 @@ class LoginController < ApplicationController
   def entrance
   end
   def login
+    unless User.find_by_email(params["email"])
+      new_user = User.create(email: params["email"], password: params["password"])
+    end
   end
 end
