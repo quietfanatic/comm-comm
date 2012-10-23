e9c4bde1795c4e32c6ace473ef11ad4833d3e68d
johnny4shure <davidshure79@gmail.com>
2012-10-23 12:52:56 -0700

    Adding confirmed user attr

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,11 +1,11 @@
 class MainController < ApplicationController
   def topic
     @user = User.find_by_session(session['session_id'])
-    if not @user
-      redirect_to '/login/entrance'
-    else
+    if @user
       @email = @user.email
       @posts = Post.order(:post_date)
+    else
+      redirect_to '/login/entrance'
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121018051305) do
+ActiveRecord::Schema.define(:version => 20121023195051) do
 
   create_table "posts", :force => true do |t|
     t.integer  "owner"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(:version => 20121018051305) do
     t.datetime "created_at",      :null => false
     t.datetime "updated_at",      :null => false
     t.string   "session"
+    t.boolean  "is_confirmed"
   end
 
 end
