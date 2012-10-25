f65eb8f53a97926a8fc61cff14b5f18b2d470fa0
Johnny <johnny@invisible.(none)>
2012-10-25 11:25:19 -0700

    More validation for post content uniqueness with the last post

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -2,7 +2,9 @@ require 'digest/sha1'
 class PostController < ApplicationController
   def new
     @user = User.find_by_session(session['session_id'])
-    Post.create(content: params['content'], post_date: DateTime.now, owner: @user.id)
+    if Post.last.content != params['content']
+      Post.create(content: params['content'], post_date: DateTime.now, owner: @user.id)
+    end
   end
 
   def edit
