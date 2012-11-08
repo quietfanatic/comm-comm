1e9178481ca7209f5136349d1eb1d57452273cea
quietfanatic <lewis@wall.org>
2012-11-07 18:51:02 -0800

    Check permissions before manipulating topics and users

--- app/controllers/topic_controller.rb
+++ app/controllers/topic_controller.rb
@@ -1,9 +1,16 @@
 class TopicController < ApplicationController
   def new
-    @name = params['name']
-    if @name
-      Topic.create(name: @name)
+    @current_user = User.find_by_session(session['session_id'])
+    if @current_user
+      if @current_user.is_confirmed and @current_user.can_edit_topics
+        @name = params['name']
+        if @name
+          Topic.create(name: @name)
+        end
+      end
+      redirect_to '/main/settings'
+    else
+      redirect_to '/login/entrance'
     end
-    redirect_to '/main/settings'
   end
 end
--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,10 +1,17 @@
 class UserController < ApplicationController
   def confirm
-    @user = User.find_by_id(params['id'])
-    if @user
-      @user.is_confirmed = true
-      @user.save!
+    @current_user = User.find_by_session(session['session_id'])
+    if @current_user
+      if @current_user.is_confirmed and @current_user.can_confirm_users
+        @user = User.find_by_id(params['id'])
+        if @user
+          @user.is_confirmed = true
+          @user.save!
+        end
+      end
+      redirect_to '/main/settings'
+    else
+      redirect_to '/login/entrance'
     end
-    redirect_to '/main/settings'
   end
 end
