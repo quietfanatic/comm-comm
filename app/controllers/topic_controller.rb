46617fc5449797266ec4e577906c43682440b2ee
quietfanatic <lewis@wall.org>
2012-11-09 12:27:12 -0800

    Topic creation event posts!

--- app/controllers/topic_controller.rb
+++ app/controllers/topic_controller.rb
@@ -4,8 +4,10 @@ class TopicController < ApplicationController
     if @current_user
       if @current_user.is_confirmed and @current_user.can_edit_topics
         @name = params['name']
-        if @name
-          Topic.create(name: @name)
+        if @name and @name =~ /\S/
+          topic = Topic.new(name: @name)
+          topic.save!
+          Post.create(post_type: Post::TOPIC_CREATION, topic: topic.id, reference: topic.id, owner: @current_user, content: topic.name)
         end
       end
       redirect_to '/main/settings'
