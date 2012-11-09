59dc0d15d4df055c9ddeb9eea7409e412c389485
quietfanatic <lewis@wall.org>
2012-11-09 11:15:25 -0800

    Make there be multiple kinds of posts.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,4 @@
 class Post < ActiveRecord::Base
-  attr_accessible :content, :owner, :post_date, :topic, :pinned
+  attr_accessible :content, :owner, :post_date, :topic, :pinned, :type, :reference
   validates :content, :presence => true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121109083246) do
+ActiveRecord::Schema.define(:version => 20121109191142) do
 
   create_table "posts", :force => true do |t|
     t.integer  "owner"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(:version => 20121109083246) do
     t.datetime "updated_at",                :null => false
     t.integer  "topic"
     t.boolean  "pinned"
+    t.integer  "type"
+    t.integer  "reference"
   end
 
   create_table "topic_users", :force => true do |t|
