b4a831fd05c3ae2fbe4e83bf73e93a503be27939
Kelly Macdonald <kmacdonald@westmont.edu>
2012-11-10 14:55:43 -0800

    just making sure

--- test/unit/post_test.rb
+++ test/unit/post_test.rb
@@ -1,16 +1,12 @@
 require 'test_helper'
-require 'capybara/rails'
-require 'selenium-webdriver'
 
 class PostTest < ActiveSupport::TestCase
 	include Capybara::DSL
-	include Selenium
-	Capybara.javascript_driver = :selenium
 	
 	test "new should exist" do
 		visit '/main/topic'
 		#fill_in 'content', :with => 'Test Content'
-		find('tubmit#new_post_submit').click
+		find('new_post_submit').click
 		assert page.has_content?('Comm-Comm'), "This page does not have the correct content."
 	end
 
