e84beaeb3172bf218adc2466a0e8dcaba8a36055
quietfanatic <lewis@wall.org>
2012-11-11 01:21:38 -0800

    Add name to signup form and perform plenty of validation.

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -16,12 +16,28 @@ class LoginController < ApplicationController
     end
   end
   def journey
-    if (params['email'] and params['password'] and params['password'] == params['confirm_password'])
-      unless User.find_by_email(params['email'])
-        @user = User.create(:email => params["email"], :password => params['confirm_password'])
-      end
-    else
+    if params['email'] !~ /\S/
+      redirect_to "/login/singup?error=Sorry,+you+must+provide+an+email+address."
+    elsif params['email'] !~ /^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9._-]$/
+      redirect_to "/login/signup?error=Sorry,+that+email+address+wasn't+accepted."
+    elsif params['name'] !~ /\S/
+      redirect_to "/login/signup?error=Sorry,+you+must+provide+a+name+for+yourself."
+    elsif params['password'] !~ /\S/
+      redirect_to "/login/signup?error=Sorry,+you+must+provide+a+password."
+    elsif params['password'].length < 6
+      redirect_to "/login/signup?error=Sorry,+your+pasword+must+be+at+least+six+characters+long."
+    elsif params['confirm_password'] !~ /\S/
+      redirect_to "/login/signup?error=Sorry,+you+must+confirm+your+password."
+    elsif params['password'] != params['confirm_password']
       redirect_to "/login/signup?error=Sorry,+your+passwords+didn't+match."
+    elsif User.find_by_email(params['email'])
+      redirect_to "/login/singup?error=Sorry,+That+email+address+is+in+use."
+    else
+      @user = User.create(
+        email: params['email'],
+        visible_name: params['name'],
+        password: params['password']
+      )
     end
   end
   def signup
