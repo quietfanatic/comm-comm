17e234ed009a3e180e15c3cb9e3f79bcc3241964
Kelly Macdonald <kmacdonald@westmont.edu>
2012-11-12 15:05:52 -0800

    just making sure

--- test/unit/post_test.rb
+++ test/unit/post_test.rb
@@ -5,20 +5,23 @@ class PostTest < ActionDispatch::IntegrationTest
 		Capybara.current_driver = Capybara.javascript_driver # :selenium by default
 	end
 	
-	test "should create new users" do
+	test "should browse site" do
 		visit '/login/entrance'
 		click_on 'request'
 		fill_in 'email', :with => 'test@test.com'
 		fill_in 'name', :with => 'test'
-		fill_in 'password', :with => 'asdf'
-		fill_in 'confirm_password', :with => 'asdf'
+		fill_in 'password', :with => 'asdfasdf'
+		fill_in 'confirm_password', :with => 'asdfasdf'
 		click_on 'submit'
-		assert page.has_content?('Comm-Comm'), "This page does not have the correct content."
+		assert page.has_content?('Your request has been submitted.'), "This page does not have the correct content."
+		User.find_by_email('test@test.com').update_attributes(:is_confirmed => true)
+		visit '/login/entrance'
+		fill_in 'email', :with => 'test@test.com'
+		fill_in 'password', :with => 'asdfasdf'
+		click_on 'enter'
+		assert page.has_content?('Uncategorized'), "This page does not have the correct content."
+		fill_in 'new_post_content', :with => 'test post'
+		click_on 'new_post_submit'
 	end
 	
-	#test "should login" do
-	#	User.find_by_email('test@test.com').update_attributes(:is_confirmed => true)
-	#	visit 'login/journey'
-	#	click_on
-	#end
 end
