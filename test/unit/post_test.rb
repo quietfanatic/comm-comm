f62cfbb082314afa687b808857b0d012dda63f6c
Kelly Macdonald <kmacdonald@westmont.edu>
2012-11-12 11:29:57 -0800

    tests signup form

--- test/unit/post_test.rb
+++ test/unit/post_test.rb
@@ -9,15 +9,16 @@ class PostTest < ActionDispatch::IntegrationTest
 		visit '/login/entrance'
 		click_on 'request'
 		fill_in 'email', :with => 'test@test.com'
+		fill_in 'name', :with => 'test'
 		fill_in 'password', :with => 'asdf'
 		fill_in 'confirm_password', :with => 'asdf'
 		click_on 'submit'
 		assert page.has_content?('Comm-Comm'), "This page does not have the correct content."
 	end
 	
-	test "should login" do
-		User.find_by_email('test@test.com').can_edit_posts == true
-		visit 'login/journey'
-		click_on
-	end
+	#test "should login" do
+	#	User.find_by_email('test@test.com').update_attributes(:is_confirmed => true)
+	#	visit 'login/journey'
+	#	click_on
+	#end
 end
