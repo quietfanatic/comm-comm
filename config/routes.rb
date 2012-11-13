e7e0a3acfaa8d57f1c4961b2bf59f267cd59b6cc
quietfanatic <lewis@wall.org>
2012-11-13 01:14:17 -0800

    Only have one form per post instead of several :)

--- config/routes.rb
+++ config/routes.rb
@@ -17,12 +17,7 @@ CommComm::Application.routes.draw do
   root :to => 'main#board'
 
   post "post/new"
-  post 'post/pin'
-  post 'post/unpin'
-  post 'post/yell'
-  post 'post/unyell'
-  post 'post/hide'
-  post 'post/unhide'
+  post 'post/button'
   post 'board/new'
   post 'board/edit'
   post 'user/confirm'
