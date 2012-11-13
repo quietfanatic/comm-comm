d95fe0cbb125e8e6415b92440437f911518a0aed
quietfanatic <lewis@wall.org>
2012-11-12 23:51:08 -0800

    Put ActionMailer config in environments/production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,3 +65,19 @@ CommComm::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 end
+
+ActionMailer::Base.raise_delivery_errors = true
+ActionMailer::Base.perform_deliveries = true
+ActionMailer::Base.logger = Rails.logger
+settings = SiteSettings.settings
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.smtp_settings = {
+  address: settings[:smtp_server],
+  port: settings[:smtp_port],
+  authentication: settings[:smtp_auto],
+  user_name: settings[:smtp_username],
+  password: settings[:smtp_password],
+  enable_starttls_auto: settings[:smtp_starttls_auto],
+  openssl_verify_mode: settings[:smtp_ssl_verify]
+}
+
