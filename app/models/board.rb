a9a9c98e13213978959e98af79fb909ad55cfe80
johnny4shure <davidshure79@gmail.com>
2012-11-14 21:43:23 -0800

    Making PPP board specific setting

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,3 +1,3 @@
 class Board < ActiveRecord::Base
-    attr_accessible :name, :last_post, :last_event, :last_yell, :order
+    attr_accessible :name, :last_post, :last_event, :last_yell, :order, :ppp
 end
