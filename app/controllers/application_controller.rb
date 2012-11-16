28d574785e10e48b903bbb9d9e8bf0439694d403
quietfanatic <lewis@wall.org>
2012-11-15 17:32:35 -0800

    Work user agents into session system and fix some bugs.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,15 +2,51 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   
   def logged_in?
-    @session = session && session.has_key?('session_id') ? Session.find_by_token(session['session_id']) : nil
-    @user = @session ? User.find_by_id(@session.user_id) : nil
-    return @session && @user && @user.is_confirmed
+    if session and session.has_key?('session_id')
+      @session = Session.where(token: session['session_id']).first
+      if @session
+         # We're suspicious if the user agent is different but the session token is not
+        if @session.user_agent.byteslice(0, 250) == request.env['HTTP_USER_AGENT'].byteslice(0, 250)
+          @user = User.find_by_id(@session.user_id)
+          if @user
+            Rails.logger.warn("Logged in user: #{@user.id} (#{@user.email})\n")
+            return true
+          else
+            Rails.logger.warn("Session did not corresponde to valid user.\n")
+            return false
+          end
+        else
+          Rails.logger.warn(
+            "User agent was changed during session; forcing logout.\n" +
+            "Old: #{@session.user_agent.byteslice(0, 250)}\n" +
+            "New: #{request.env['HTTP_USER_AGENT'].byteslice(0, 250)}\n"
+          )
+          @session.destroy  # Though the usual cause will be browser upgrade
+          @session = nil
+          @user = nil
+          return false
+        end
+      else
+        Rails.logger.warn("Not logged in.\n")
+        @session = nil
+        @user = nil
+        return false
+      end
+    else
+      Rails.logger.warn("Not logged in.\n")
+      @session = nil
+      @user = nil
+      return false
+    end
   end
 
   def logged_in (&block)
     if logged_in?
       block.call
       return true
+    elsif @user
+      redirect_to '/login/journey'
+      return false
     else
       redirect_to '/login/entrance'
       return false
