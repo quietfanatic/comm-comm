64749171d18e266bfdaff277620b17afd1066846
Kelly Macdonald <kmacdonald@westmont.edu>
2012-11-22 20:40:47 -0800

    just making sure

--- test/unit/post_test.rb
+++ test/unit/post_test.rb
@@ -7,7 +7,11 @@ class PostTest < ActionDispatch::IntegrationTest
 	
 	test "should create new users" do
 		user = User.create(:email => 'test@test.com', :username => 'test', :password => 'asdfasdf', :is_confirmed => true)
-		visit 'main/board'
+		visit 'rack_session/edit'
+		visit '/login/entrance'
+		fill_in 'email', :with => 'test@test.com'
+		fill_in 'password', :with => 'asdfasdf'
+		click_on 'enter'
 		assert page.has_content?('Comm Comm'), "This page does not have the correct content."
 	end
 end
