a19fbb52c39503a5fa634c55faa1a869738ecb14
Kelly Macdonald <kmacdonald@westmont.edu>
2012-12-01 20:23:40 -0800

    just making sure like I always am

--- test/unit/post_test.rb
+++ test/unit/post_test.rb
@@ -20,4 +20,20 @@ class PostTest < ActionDispatch::IntegrationTest
 		click_on 'new_post_submit'
 		assert page.has_content?('Test Post'), "Posting did not work."
 	end
+	
+	test "should hide posts" do
+		user = User.create(:email => 'test@test.com', :username => 'test', :password => 'asdfasdf', :is_confirmed => true)
+		user.session = 1
+		user.save!
+		cookies['_comm_comm_session'] = 1
+		page.set_rack_session(:session_id => 1, :user_id => user.id)
+		visit '/login/entrance'
+		fill_in 'email', :with => 'test@test.com'
+		fill_in 'password', :with => 'asdfasdf'
+		click_on 'enter'
+		visit '/main/board'
+		post = Post.find(1)
+		click_on 'hide_button'
+		assert (find('##{post}').className =~ /hidden/)
+	end
 end
