6190f3870e99933bb0a2063d77864bf56a8c4712
quietfanatic <lewis@wall.org>
2013-01-08 19:26:12 -0800

    Dodge massive security problem that was just discovered in rails.

--- config/environment.rb
+++ config/environment.rb
@@ -4,3 +4,6 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 CommComm::Application.initialize!
 
+# Safety against a security exploit in parameter passing.
+#  https://groups.google.com/forum/?fromgroups=#!topic/rubyonrails-security/61bkgvnSGTQ
+ActionDispatch::ParamsParser::DEFAULT_PARSERS.delete(Mime::XML)
